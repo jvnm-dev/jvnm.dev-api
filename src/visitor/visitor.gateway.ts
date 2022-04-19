75e830999aba15489ec12898876c396ca09b00af
Jason Van Malder <jason.vanmalder@haulogy.net>
2022-04-19 20:18:57 +0200

    feat(visitor): improve visitor feature

--- src/visitor/visitor.gateway.ts
+++ src/visitor/visitor.gateway.ts
@@ -7,10 +7,14 @@ import {
 } from '@nestjs/websockets'
 import { Logger } from '@nestjs/common'
 import { Socket, Server } from 'socket.io'
+import {
+    uniqueNamesGenerator,
+    adjectives,
+    colors,
+    animals,
+} from 'unique-names-generator'
 
 const randomColor = (() => {
-    'use strict'
-
     const randomInt = (min, max) => {
         return Math.floor(Math.random() * (max - min + 1)) + min
     }
@@ -29,47 +33,64 @@ const randomColor = (() => {
     },
 })
 export class VisitorGateway
-    implements OnGatewayConnection, OnGatewayDisconnect {
+    implements OnGatewayConnection, OnGatewayDisconnect
+{
+    getSockets() {
+        return Array.from(this.server.sockets.sockets.values())
+    }
+
     @WebSocketServer() server: Server
     private logger: Logger = new Logger('VisitorGateway')
 
-    @SubscribeMessage('positionChangedServer')
-    handlePositionChanged(
-        client: Socket,
-        payload: { id: number; x: number; y: number }
-    ): void {
-        this.logger.log(
-            `Client ${client.id} moved to ${payload.x}, ${payload.y}`
-        )
-        this.server.emit('positionChangedClient', {
-            id: client.id,
-            color: client.data.color,
-            ...payload,
-        })
-    }
+    handleConnection(client: Socket) {
+        this.logger.log(`Client connected: ${client.id}`)
 
-    @SubscribeMessage('disconnectToServer')
-    disconnectToServer(client: Socket) {
-        this.server.emit('disconnectToClient', {
+        client.data = {
+            color: randomColor(),
+            name: uniqueNamesGenerator({
+                dictionaries: [adjectives, colors, animals],
+            }),
+        }
+
+        this.server.emit('joined', {
             id: client.id,
+            x: 0,
+            y: 0,
+            ...client.data,
         })
     }
 
     handleDisconnect(client: Socket) {
         this.logger.log(`Client disconnected: ${client.id}`)
+
+        const everyoneExceptClient = this.getSockets()
+            .filter(({ id }) => id !== client.id)
+            .map(({ id }) => id)
+
+        this.server.to(everyoneExceptClient).emit('leaved', {
+            id: client.id,
+            ...client.data,
+        })
     }
 
-    handleConnection(client: Socket) {
-        this.logger.log(`Client connected: ${client.id}`)
-        client.data = {
-            color: randomColor(),
+    @SubscribeMessage('update')
+    handleUpdate(
+        client: Socket,
+        payload: {
+            id: number
+            x: number
+            y: number
+            color: string
+            name: string
+            location: string
         }
+    ): void {
+        this.logger.log(`Client ${client.data.name} updated`)
 
-        this.server.emit('connected', {
+        this.server.emit('update', {
             id: client.id,
-            color: client.data.color,
-            x: 0,
-            y: 0,
+            ...payload,
+            ...client.data,
         })
     }
 }
